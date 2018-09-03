c9abf64f4ad6e3cf54fb60951354e8c70a11a479
Administrator <965621112@qq.com>
2018-09-03 20:09:13 +0800

    后台管理系统

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,21 +1,38 @@
 package com.taotao.controller;
 
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class ItemController {
 
     @Autowired
     private ItemService itemService;
 
+    @RequestMapping("/say")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello";
+    }
+
     @RequestMapping("/item/{itemId}")
     @ResponseBody
-    public TbItem getItemById(@PathVariable Long itemId){
-        TbItem item= itemService.getItemById(itemId);
+    public TbItem getItemById(@PathVariable Long itemId) {
+        TbItem item = itemService.getItemById(itemId);
         return item;
+    }
+
+    @RequestMapping("/item/list")
+    @ResponseBody
+    public EasyUIDataGridResult getItemList(int page, int rows) {
+        EasyUIDataGridResult result = itemService.getItemList(page, rows);
+        return result;
+
     }
 }
