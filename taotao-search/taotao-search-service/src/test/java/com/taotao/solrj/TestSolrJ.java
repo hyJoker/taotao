d8fbe9571e1487eb2d8c6182d175433df0e2954a
Administrator <965621112@qq.com>
2018-11-14 23:36:58 +0800

    搭建solr搜索服务

--- taotao-search/taotao-search-service/src/test/java/com/taotao/solrj/TestSolrJ.java
+++ taotao-search/taotao-search-service/src/test/java/com/taotao/solrj/TestSolrJ.java
@@ -11,6 +11,8 @@ import org.apache.solr.common.SolrInputDocument;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class TestSolrJ {
 
@@ -66,6 +68,51 @@ public class TestSolrJ {
             String id = document.getFieldValue("id").toString();
             String item_title = document.getFieldValue("item_title").toString();
             System.out.println("id:" + id + "------" + "item_title:" + item_title);
+        }
+    }
+
+    @Test
+    public void queryDocumentWithCondition() throws SolrServerException {
+        HttpSolrServer solrServer = new HttpSolrServer("http://192.168.119.138:8080/solr/collection1");
+        //创建一个solrQuery对象
+        SolrQuery query = new SolrQuery();
+        //设置查询条件、过滤条件、分页条件、排序条件、高亮
+        query.set("q", "*:*");
+        query.setQuery("手机");
+        //分页条件
+        query.setStart(10);
+        query.setRows(3);
+        //设置默认搜索域
+        query.set("df", "item_keywords");
+        //设置高亮
+        query.setHighlight(true);
+        //设置高亮显示的域
+        query.addHighlightField("item_title");
+        query.setHighlightSimplePre("<em>");
+        query.setHighlightSimplePost("</em>");
+        //执行查询,得到一个response对象
+        QueryResponse response = solrServer.query(query);
+        //去查询结果
+        SolrDocumentList solrDocumentList = response.getResults();
+        //去查询结果总数
+        System.out.println("查询总记录数:" + solrDocumentList.getNumFound());
+        for (SolrDocument document : solrDocumentList) {
+            System.out.println(document.getFieldValue("id"));
+            //取高亮显示
+            Map<String, Map<String, List<String>>> highlighting = response.getHighlighting();
+            List<String> list = highlighting.get(document.getFieldValue("id")).get("item_title");
+            String itemTitle = "";
+            if (list != null && list.size() > 0) {
+                itemTitle = list.get(0);
+            } else {
+                itemTitle = (String) document.get("item_title");
+            }
+            System.out.println(itemTitle);
+            System.out.println(document.get("item_sell_point"));
+            System.out.println(document.get("item_price"));
+            System.out.println(document.get("item_image"));
+            System.out.println(document.get("item_category_name"));
+            System.out.println("===============================================");
         }
     }
 }
